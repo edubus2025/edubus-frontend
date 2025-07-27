a180c85dd0cab67b0893784d3f6040c70fd529ba
najmi <opentice2023@gmail.com>
2025-07-27 21:09:44 +0100

    🐛 Correction affichage mobile : ajout de meta viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="fr" suppressHydrationWarning>
+      <head>
+        {/* ✅ Balise meta viewport pour adaptabilité mobile et WebView */}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           {children}
